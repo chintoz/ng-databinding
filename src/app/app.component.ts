05fe084ba4d9ae794d2eb35c8cca1f203e28e5cb
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2022-01-22 13:43:42 +0100

    Changes to adapt initial project, to the based project needed in databinding section.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-databinding';
+  serverElements = [];
+  newServerName: string = '';
+  newServerContent: string = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
 }
