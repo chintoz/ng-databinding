9d6b70e9d554020a8813cee676bbc8217ef14a23
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2022-01-24 22:52:17 +0100

    Changes to use all existing lifecycle hooks.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges, OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
 // It's possible to use encapsulation attribute to control how it's encapsulated at browser level: None, Emulated or ShadowDom
 // encapsulation: ViewEncapsulation.Emulated
@@ -7,13 +16,46 @@ import {Component, Input, OnInit} from '@angular/core';
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy {
   @Input('srvElement') element: { type: string, name: string, content: string }
+  @Input() name: string
 
   constructor() {
+    console.log("constructor called!!")
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked called")
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit called")
   }
 
   ngOnInit(): void {
+    console.log("ngOnInit called!!")
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges called")
+    console.log(changes)
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck called")
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit called")
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked called")
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy called")
   }
 
 }
